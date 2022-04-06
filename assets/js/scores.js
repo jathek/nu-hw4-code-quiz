0b482aca5aea6590dd5d9bb01de6c8a43b7e8345
nearcatch <692914+nearcatch@users.noreply.github.com>
2022-04-05 23:46:55 -0500

    comments

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,28 +1,35 @@
-let scores = JSON.parse(localStorage.getItem("scores"));
+// read current scores from localStorage
 
+let scores = JSON.parse(localStorage.getItem("scores"));
 let quizBody = document.querySelector(".quiz-body");
+// if scores are empty, write card accordingly
 if (scores === null) {
     let scoresEmpty = document.createElement("p");
     scoresEmpty.innerText = "No scores yet!";
     quizBody.appendChild(scoresEmpty);
 } else {
+    // write ul to hold score list
+    let scoresList = document.createElement("ul");
+    scoresList.setAttribute("id","scores-list")
+    // write li elements for each score
+    quizBody.appendChild(scoresList);
     for (let i = 0; i < scores.length; i++) {
         let scoreEntry = document.createElement("li");
-        let scoresList = document.createElement("ul");
         let name = scores[i].name;
         let score = scores[i].score;
         scoreEntry.innerText = `${name}: ${score}`;
-        quizBody.appendChild(scoresList);
         scoresList.appendChild(scoreEntry);
     }
 }
 
+// add click listener for #go-home to redirect to quiz start
 let homeButton = document.querySelector("#go-home");
-let clearButton = document.querySelector("#clear-scores");
-
 homeButton.addEventListener("click", function (event) {
     window.location.href = "./index.html";
 });
+
+// add click listener for #clear-scores to clear localStorage of scores
+let clearButton = document.querySelector("#clear-scores");
 clearButton.addEventListener("click", function (event) {
-    localStorage.clear();
+    localStorage.removeItem("scores");
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// questions array
 let questions = [
     {
         question: "Commonly used data types DO NOT include:",
@@ -11,9 +12,13 @@ let questions = [
         answer: "2",
     },
     {
-        question:
-            "Arrays in JavaScript can be used to store ______.",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        question: "Arrays in JavaScript can be used to store ______.",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above",
+        ],
         answer: "3",
     },
     {
@@ -27,9 +32,9 @@ let questions = [
             "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["console log", "terminal/bash", "for loops", "JavaScript"],
         answer: "0",
-    }
+    },
 ];
-
+// establish initial variables
 let quizHeader = document.querySelector(".quiz-header");
 let quizBody = document.querySelector(".quiz-body");
 let quizfooterMessage = document.querySelector(".quiz-footer p");
@@ -38,6 +43,7 @@ let timerSecondsLeft = 75;
 let timer;
 let timerSpan = document.querySelector("#timeLeft");
 
+// add click to start button to start quiz
 const startButton = document.querySelector("button.start");
 startButton.addEventListener("click", startQuiz);
 function startQuiz(event) {
@@ -46,6 +52,7 @@ function startQuiz(event) {
         timerSecondsLeft--;
         timerSpan.innerText = `${timerSecondsLeft} seconds left.`;
         if (timerSecondsLeft <= 0) {
+            // if timer ends or goes negative, end quiz and write score form
             clearInterval(timer);
             timerSpan.innerHTML = "Time expired";
             writeScoreForm();
@@ -53,29 +60,32 @@ function startQuiz(event) {
     }, 1000);
     // create html for first question
     writeQuestion(0);
-    // debug shortcut - REMOVE below
-    // clearInterval(timer);
-    // writeScoreForm();
 }
-
+// add click to advance quiz to all quiz buttons
 quizBody.addEventListener("click", advanceQuiz);
 function advanceQuiz(event) {
     if (event.target.matches("button.quiz-choice")) {
+        // get current question and choice index from clicked button
         let currentQuestionIndex = event.target.dataset.question;
         let choiceIndex = event.target.dataset.choice;
+        // get answer using currentQuestionIndex
         let currentAnswerIndex = questions[currentQuestionIndex].answer;
+        // compare choice and answer, write footer, and modify timer accordingly
         if (choiceIndex === currentAnswerIndex) {
             quizfooterMessage.innerText = "Correct!";
         } else {
             quizfooterMessage.innerText = "Wrong!";
             timerSecondsLeft -= 10;
         }
+        // empty footer after 1 second
         let hideFooter = setTimeout(function () {
             quizfooterMessage.innerText = "";
         }, 1000);
+        // advance currentQuestionIndex and write next question if still within array
         currentQuestionIndex++;
         if (currentQuestionIndex < questions.length) {
             writeQuestion(currentQuestionIndex);
+            // if currentQuestionIndex outside array, clear timer and write score form
         } else {
             clearInterval(timer);
             timerSpan.innerText = `${timerSecondsLeft} seconds left.`;
@@ -83,9 +93,12 @@ function advanceQuiz(event) {
         }
     }
 }
+// write question card using questionIndex received from advanceQuiz
 function writeQuestion(questionIndex) {
     let quizChoiceOl = document.createElement("ol");
+    // write question to header
     quizHeader.innerHTML = `<h2>${questions[questionIndex].question}</h2>`;
+    // iterate through current question object and write li and button elements for choices
     quizBody.innerHTML = "";
     quizBody.appendChild(quizChoiceOl);
     for (let i = 0; i < questions[questionIndex].choices.length; i++) {
@@ -99,28 +112,36 @@ function writeQuestion(questionIndex) {
         choiceLi.append(choiceButton);
     }
 }
+// write score form
 function writeScoreForm() {
-    quizHeader.innerHTML = "<h2>Submit your score!</h2>";
+    quizHeader.innerHTML = `<h2>Finished with ${timerSecondsLeft} seconds left. Submit your score!</h2>`;
     quizBody.innerHTML =
         '<form id="score-form"><input type="text"><button class="score">Submit</button></form>';
 }
-
+// submit listener for score submit button
 quizBody.addEventListener("submit", saveScore);
 function saveScore(event) {
     if (event.target.matches("#score-form")) {
         event.preventDefault();
         let scoreForm = quizBody.querySelector("#score-form");
+        // use time remaining as score
         let currentScore = timerSecondsLeft;
+        // get current input
         let nameInput = scoreForm.querySelector("input");
         let name = nameInput.value.trim();
+        // create object for current score and name
         let currentRecord = { score: currentScore, name: name };
+        // get stored scores from localStorage
         let storedScores = JSON.parse(localStorage.getItem("scores"));
         if (storedScores !== null) {
             scores = storedScores;
         }
+        // push current score to scores
         scores.push(currentRecord);
+        // store scores in localStorage
         localStorage.setItem("scores", JSON.stringify(scores));
         nameInput.value = "";
+        // redirect to scores page
         window.location.href = "./scores.html";
     }
 }
